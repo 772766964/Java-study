c45d88c8d7d6f7692fdc449098570d8c6a79bbfc
772766964 <772766964@qq.com>
2020-10-24 23:21:34 +0800

    2020年10月24日

--- code/java-study/src/main/java/com/daily/Day5/TraversalList.java
+++ code/java-study/src/main/java/com/daily/Day5/TraversalList.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class TraversalList {
     public static void main(String[] args) {
+        
         List<String> list = new ArrayList<>(Arrays.asList("tom","cat","jane","jerry"));
 
         //方法1 集合类的通用遍历方式，从很早的版本就有，用迭代器迭代
@@ -47,5 +48,6 @@ public class TraversalList {
 
         //方法6 终极简洁版(方法引用)
         list.forEach(System.out::print);
+
     }
 }
