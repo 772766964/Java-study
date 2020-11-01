6683ba2f4049643dcf45ca52a5390351401554dd
772766964 <772766964@qq.com>
2020-11-01 22:53:13 +0800

    2020年11月1日

---
+++ code/java-study/src/main/java/com/daily/Day13/TestSystem.java
@@ -0,0 +1,25 @@
+package com.daily.Day13;
+
+/**
+ * @ClassName TestSystem
+ * @Description TODO
+ * @Author 1
+ * @Date 2020/11/1
+ **/
+public class TestSystem {
+
+    private static final int MAX = 10000;
+
+    public static void main(String[] args) {
+        //获取当前时间毫秒值
+        System.out.println(System.currentTimeMillis());
+        //验证for循环打印数字1-9999所需要使用的时间(毫秒)
+        long star = System.currentTimeMillis();
+        for (int i = 0;i<MAX;i++){
+            System.out.println(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("共耗时："+(end - star)+" 毫秒");
+    }
+
+}
